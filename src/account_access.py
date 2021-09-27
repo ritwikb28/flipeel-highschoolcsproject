aba7ea844f11bb92ef4d8153028897bdf56e147d
banana28chips <ritwik28102004@gmail.com>
2021-09-27 09:02:19 +0530

    new

--- src/account_access.py
+++ src/account_access.py
@@ -4,13 +4,13 @@ con = mysqltr.connect(host = "localhost", user = "root", password = "ritwik", da
 
 hello = input("do you already have an account? (y/n)")
 if hello == "y" :
-        acc = print('''do you want to create a product list? *beta* 
+        acc = input('''do you want to create a product list? *beta* 
         proceed to dashboard? (pl/d)''')
         if acc == "d":
             name = input("account name:")
             cursor = con.cursor()
             sql = "select * from user_data where Name = %s "
-            cursor.execute(sql, name)
+            cursor.execute(sql, [name])
             hi = input("do you intend to update acc details or delete? (u/d)")
             if hi == "u":
                 change = input('''what do you want to change:
@@ -18,20 +18,29 @@ if hello == "y" :
                 PhoneNumber (p)
                 EmailId (e)''')
                 if change == "n":
+                    ph = int(input("confirm your phone number"))
                     updated_n = input("updated name:")
-                    sql1 = "update user_data SET Name = %s"
-                    cursor.execute(sql1, (updated_n,name))
-                    #con.commit()
+                    sql1 = "update user_data SET Name = %s WHERE PhoneNumber = %a"
+                    y = cursor.execute(sql1, ([updated_n],ph))
+                    con.commit()
+                    if y == 1:
+                        print("detail updated")
                 elif change == "p":
-                    updated_p = int(input("updated phone number"))
-                    sql1 = "update user_data SET PhoneNumber = %s"
-                    cursor.execute(sql1, (updated_p,name))
-                    #con.commit()
+                    ph = int(input("confirm your phone number"))
+                    updated_p = int(input("updated phone:"))
+                    sql1 = "update user_data SET PhoneNumber = %s WHERE PhoneNumber = %a"
+                    y = cursor.execute(sql1, ([updated_p],ph))
+                    con.commit()
+                    if y == 1:
+                        print("detail updated")
                 elif change == "e":
-                    updated_id = int(input("updated EmailId"))
-                    sql1 = "update user_data SET EmailId = %s WHERE Name = %s"
-                    cursor.execute(sql1, (updated_id,name))
-                    #con.commit
+                    ph = int(input("confirm your phone number"))
+                    updated_id = input("updated emailid:")
+                    sql1 = "update user_data SET EmailId = %s WHERE PhoneNumber = %a"
+                    y = cursor.execute(sql1, ([updated_id],ph))
+                    con.commit()
+                    if y == 1:
+                        print("detail updated")
             elif hi == "d":
                 delete = input('''what do you want to change:
                 Name (n)
